b284b30b1c8a36b800f9e98210bf92a7b8b70ac5
Джон, просто Джон <smd.deluzion@gmail.com>
2023-06-25 16:42:57 +0200

    Add godoc

--- api.go
+++ api.go
@@ -1,5 +1,12 @@
 package gopublicpackageexercise
 
+// Add function takes two input arguments and returns their sum
+//
+// It has two parameters: a and b, both ints
+// No error is returned
+// More info on addition at [mathisfun]
+//
+// [mathisfun]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a int, b int) int {
 	return a + b
 }
---
+++ doc.go
@@ -0,0 +1,2 @@
+// go_public_package_exercise is a sample package to exercise the distribution of packaged logic for Golang
+// its API provides only one function Add
